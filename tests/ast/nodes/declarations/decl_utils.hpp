2485321fb372bcb95d6a8a4b2131be31881748b9
dyb <david.yves.bachmann@gmail.com>
2025-12-07 20:35:30 +0000

    .

--- tests/ast/nodes/declarations/decl_utils.hpp
+++ tests/ast/nodes/declarations/decl_utils.hpp
@@ -1,7 +1,6 @@
 #ifndef DECL_UTILS_HPP
 #define DECL_UTILS_HPP
 
-#include "ast/nodes/declarations.hpp"
 #include "ast/nodes/design_file.hpp"
 #include "ast/nodes/design_units.hpp"
 #include "builder/ast_builder.hpp"
